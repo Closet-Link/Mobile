018800af795d38b006d856ffb48f23c4d499d08f
0Hooni <thddudgns972@gmail.com>
2025-05-22 01:42:05 +0900

    feat: 기본적인 첫 화면 구현

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
 import Text from "./components/Text";
 import colors from "./styles/colors";
 
+// 이미지 import 예시
+import closetIllustration from '../assets/images/illustrations/closet_illustration.png';
+
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text variant="title1" color={colors.primary1}>
-        회원가입
+    <SafeAreaView style={styles.container}>
+      <Image 
+        source={closetIllustration}
+        style={styles.illustration}
+      />
+      <Text variant="title1" color={colors.textPrimary} style={styles.title}>
+        {'당신의 옷장\n누군가의 스타일이 되다'}
       </Text>
-    </View>
+      <View>
+        <TouchableOpacity onPress={() => { console.log("시작하기") }}>
+          <Text variant="body1" color={colors.textPrimary}>
+            시작하기
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { console.log("둘러보기") }}>
+          <Text variant="body1" color={colors.textPrimary}>
+            둘러보기
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -19,4 +38,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: colors.gray1,
   },
-});
+  illustration: {
+    width: 192,
+    height: 192,
+    marginBottom: 80,
+  },
+  title: {
+    textAlign: 'center'
+  },
+}); 
